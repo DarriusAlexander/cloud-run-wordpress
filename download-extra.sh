b8c8712c3c967028247e901b3c8290c1689c7652
Darrius Alexander <darriush@voltlighting.com>
2020-08-04 02:25:31 -0400

    Update download-extra.sh
    ..

--- download-extra.sh
+++ download-extra.sh
@@ -2,3 +2,11 @@
 curl -LO https://github.com/DarriusAlexander/speaklight-content/archive/master.zip  | grep -oP '"tag_name": "\K(.*)(?=")'
 unzip master.zip -d /wordpress
 mv /wordpress/wp-content-master /wordpress/wp-content
+
+
+
+RUN curl -LO https://github.com/DarriusAlexander/speaklight-content/archive/master.zip  | grep -oP '"tag_name": "\K(.*)(?=")'; \
+	unzip master.zip -d /usr/src/wordpress/wp-content; \
+	mv /usr/src/wordpress/wp-content-master /usr/src/wordpress; \
+	chown -R www-data:www-data wp-content; \
+	chmod -R 777 wp-content
